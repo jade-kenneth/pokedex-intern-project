1a29b7c70c225a1fe528f60fe765dd4ffe4188b1
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-06-14 20:03:19 +0800

    added google analytics config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,6 +46,17 @@ fbq('track', 'PageView');
               `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRVBNR5');
+              `,
+            }}
+          />
 
           <script
             dangerouslySetInnerHTML={{
@@ -84,17 +95,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '1286118985116691');
   fbq('track', 'PageView');
-              `,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            !function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NRVBNR5');
               `,
             }}
           />
